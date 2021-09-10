ec0b192061b11f467cafc1f2df83b88af6a52a9e
arland <arland.torres@outlook.com>
2021-09-11 00:57:24 +0100

    Implement base.html template
    - Add Bootstrap 5.1.1 via CDN
    - Responsive nav bar & sticky footer
    - Highlights active page

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return render_template("index.html")
+def home():
+    return render_template("index.html", page_title="Home")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html", page_title="About")
+
+
+@app.route('/faq')
+def faq():
+    return render_template("faq.html", page_title="FAQ")
 
 
 if __name__ == "__main__":
