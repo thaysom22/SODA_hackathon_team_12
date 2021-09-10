68a4efc35519aa85397574bb287de5bf020c2d06
seanyoung247 <seanyoung247@gmail.com>
2021-09-10 22:39:21 +0100

    feat: Adds user login decorator

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
-from flask import Flask
+import os
+from functools import wraps
+from flask import (Flask, redirect, url_for, session)
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
+
+
+def requires_user(func):
+    """
+    Redirects wrapped route to user log in if no user in session.
+    """
+    @wraps(func)
+    def route(*args, **kwargs):
+        if session.get("user") is None:
+            # Display flash message?
+            return redirect(url_for("user"))
+        else:
+            return func(*args, **kwargs)
+
+    return route
+
 
 @app.route("/")
 def hello_world():
