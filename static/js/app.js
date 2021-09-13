8d63b84f02d0c4e3ea534055578cbe8ab0e0d2f7
seanyoung247 <seanyoung247@gmail.com>
2021-09-13 08:57:51 +0100

    feat: Hides other popovers when one is shown

--- static/js/app.js
+++ static/js/app.js
@@ -10,3 +10,15 @@ var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
 // Allow buttons elements in popovers
 var allowList = bootstrap.Popover.Default.allowList;
 allowList.button = ["data-bs-toggle", "data-bs-target"];
+
+ /* Hides other popovers on show - bit of a hack but best I can do for now
+    Ideally we should trigger popovers manually and have node effects toggled
+    with classes rather than focus. Then we can target the classes for showing
+    or hiding the popovers. */
+popoverList.forEach(function (popover) {
+  // Attach show event to each trigger
+  popover._element.addEventListener('show.bs.popover', function(e) {
+    // If a popover is being shown, hide all others.
+    popoverList.forEach((item) => {if (item != popover) item.hide();});
+  });
+});
